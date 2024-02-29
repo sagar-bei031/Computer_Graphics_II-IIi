0233a73ba40fdd1c6a20e30154b298f94f25155b
Sagar Chaudhary <078bei031.sagar@pcampus.edu.np>
2024-02-29 13:52:55 +0545

    Fix MEA [float to int] and add debug config

--- src/MEA.cpp
+++ src/MEA.cpp
@@ -5,11 +5,11 @@ void MEA::drawEllipse(SDL_Renderer *renderer, int xc, int yc, int rx, int ry)
 {
     int x = 0;
     int y = ry;
-    float rx2 = rx * rx;
-    float ry2 = ry * ry;
-    float px = 0;
-    float py = 2 * rx2 * y;
-    float p;
+    int rx2 = rx * rx;
+    int ry2 = ry * ry;
+    int px = 0;
+    int py = 2 * rx2 * y;
+    int p;
 
     // Draw starting point
     renderEllipse(renderer, xc, yc, x, y);
@@ -58,8 +58,8 @@ void MEA::drawEllipse(SDL_Renderer *renderer, int xc, int yc, int rx, int ry)
 
 inline void MEA::renderEllipse(SDL_Renderer *renderer, int xc, int yc, int x, int y)
 {
-    SDL_RenderDrawPoint(renderer, xc + x, yc - y);
-    SDL_RenderDrawPoint(renderer, xc - x, yc - y);
     SDL_RenderDrawPoint(renderer, xc + x, yc + y);
+    SDL_RenderDrawPoint(renderer, xc + x, yc - y);
     SDL_RenderDrawPoint(renderer, xc - x, yc + y);
+    SDL_RenderDrawPoint(renderer, xc - x, yc - y);
 }
